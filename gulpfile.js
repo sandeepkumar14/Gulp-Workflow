c90a7f4a01175f062f1bdb3e6e0d1c9e4eed89ad
Sandeep Kumar <sandeep47momi@gmail.com>
2016-08-17 16:56:33 +0200

    Added sample js files and creted first concatenation task for js files.

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,34 @@ var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 var autoprefixer= require('gulp-autoprefixer');
 var rimraf = require('rimraf');
+
+//«««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+// Concatenation of all the custom javaScript files (for development)
+//«««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+var jsSrc = [
+               "./development/components/javascript/sample1.js",
+               "./development/components/javascript/sample2.js",
+               "./development/components/javascript/sample3.js"
+            ];
+var jsDest = "./development/javascript/";
+//Task
+gulp.task('concatJs', function(){
+   gulp.src(jsSrc)
+    .pipe(plumber({
+        errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+      }}))
+     .pipe(concat('script.js'))
+     .on('error', function(err) {
+        console.log(err);
+        this.emit('end');
+      })  
+     .pipe(gulp.dest(jsDest))
+     .pipe(reload({stream:true}));
+});
+
+//««««««««««««««««««««
+// gulp Default task
+//««««««««««««««««««««
+gulp.task('default', ['concatJs']);
