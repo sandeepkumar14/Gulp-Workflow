cb125dfe4c1d083e0807cbbfa28649b278a1b3ae
Sandeep Kumar <sandeep47momi@gmail.com>
2016-09-02 00:11:09 +0200

    Gulp updates, replaces compass by gulp sass, add source map plugin, del task over rimraf, and updated the workflow file system.

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,133 @@
+/* jshint esnext: true*/
 //««««««««««««««««««««
 // Required Modules 
 //««««««««««««««««««««
 var gulp= require('gulp');
 var concat= require('gulp-concat');
-var compass= require('gulp-compass');
+var sass= require('gulp-sass');
 var uglify= require('gulp-uglify');
-var minifyCSS = require('gulp-minify-css');
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
 var htmlmin = require('gulp-htmlmin');
 var plumber= require('gulp-plumber');
 var browserSync = require('browser-sync').create();
+var autoprefixer= require('gulp-autoprefixer'); 
 var reload = browserSync.reload;
-var autoprefixer= require('gulp-autoprefixer');
-var rimraf = require('rimraf');
+var del = require('del');
 
-//«««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
-// Concatenation of all the custom javaScript files (for development)
-//«««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
-var jsSrc = [
-               "./development/components/javascript/sample1.js",
-               "./development/components/javascript/sample2.js",
-               "./development/components/javascript/sample3.js"
-            ];
+
+//////////////////////////////////////////////////////////
+/* «««««««««««««««« DEVELOPMENT ««««««««««««««««««««««««*/
+//////////////////////////////////////////////////////////
+
+// SASS Task
+//««««««««««««««««««««««««««««
+var sassSrc = ['./components/sass/style.scss'];
+var sassDest = './development/css/';
+// Task
+gulp.task('sass', function(){
+   return gulp.src(sassSrc)
+      .pipe(sourcemaps.init({loadMaps: true, debug: true, identityMap: true}))
+      .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+      .pipe(plumber({            // plumber
+        errorHandler: onError
+      }))
+      .pipe(autoprefixer({
+        browser: ['last 2 versions'],
+        cascade: false
+      }))
+      .pipe(sourcemaps.write('../maps/css/'))
+      .pipe(gulp.dest(sassDest))
+      .pipe(plumber({            // plumber
+        errorHandler: onError
+      }))
+      .pipe(reload({stream:true}));
+});
+
+
+// gulp HTML task
+//««««««««««««««««««««
+var htmlSrc = './development/index.html';
+// Task
+gulp.task('html', function(){
+  gulp.src(htmlSrc)
+  .pipe(plumber({            // plumber
+        errorHandler: onError
+    }))
+  .pipe(reload({stream:true}));
+});
+
+/* Custom Javascript files Concatenation */
+/*««««««««««««««««««««««««««««««««««««««««««««««««*/
+var jsSrc= [
+  "./components/javascript/sample1.js",
+  "./components/javascript/sample2.js",
+];
 var jsDest = "./development/javascript/";
+
 //Task
 gulp.task('concatJs', function(){
    gulp.src(jsSrc)
-    .pipe(plumber({
-        errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-      }}))
-     .pipe(concat('script.js'))
-     .on('error', function(err) {
-        console.log(err);
-        this.emit('end');
-      })  
-     .pipe(gulp.dest(jsDest))
-     .pipe(reload({stream:true}));
+    .pipe(sourcemaps.init())   // source maps
+    .pipe(plumber({            // plumber
+        errorHandler: onError
+    }))
+    .pipe(concat('script.js'))
+    .pipe(plumber({            // plumber
+        errorHandler: onError
+    }))  
+    .pipe(sourcemaps.write('../maps/js/')) 
+    .pipe(gulp.dest(jsDest))
+    .pipe(reload({stream:true}));
 });
 
-//««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
-// Concatenation of all the javaScript libraries (for development)
-//««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+/* Concatenation of all javascript Libraries */
+/*««««««««««««««««««««««««««««««««««««««««««««««««*/
 // javaScript Libraries Path
 var libSrc= [ 
-    './development/components/lib/angular/angular.js', 
-    './development/components/lib/angular-resource/angular-resource.js',
-    './development/components/lib/angular-route/angular-route.js',
-    './development/components/lib/jquery/index.js', 
-    './development/components/lib/jquery-migrate/index.js',
-    './development/components/lib/bootstrap/dist/js/bootstrap.js'
+    './components/lib/angular/angular.js', 
+    './components/lib/angular-resource/angular-resource.js',
+    './components/lib/angular-route/angular-route.js',
+    './components/lib/jquery/index.js', 
+    './components/lib/jquery-migrate/index.js',
+    './components/lib/bootstrap/dist/js/bootstrap.js'
 ];
 var libDest = './development/lib/'; 
+
 //Task
 gulp.task('concatLibs', function(){
-   gulp.src(libSrc)
-     .pipe(concat('libraries.js'))
-     .pipe(gulp.dest(libDest));
+  gulp.src(libSrc)
+    .pipe(plumber({            // plumber
+        errorHandler: onError
+    }))
+    .pipe(concat('libraries.js'))
+    .pipe(plumber({            // plumber
+        errorHandler: onError
+    }))
+    .pipe(gulp.dest(libDest));
 });
 
 
-//«««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
-// Concatenation of all the minifies javaScripts (for production)
-//«««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
-// JS Libraries Minified files path
-var libMinSrc = [
-    './development/components/lib/angular/angular.min.js', 
-    './development/components/lib/angular-resource/angular-resource.min.js',
-    './development/components/lib/angular-route/angular-route.min.js',
-    './development/components/lib/jquery_min/index.js',
-    './development/components/lib/jquery-migrate_min/index.js',
-    './development/components/lib/bootstrap/dist/js/bootstrap.min.js'
-];
-var libMinDest = './production/lib/';
-//Task
-gulp.task('concatMinLibs', function(){
-   gulp.src(libMinSrc)
-     .pipe(concat('libraries.js'))
-     .pipe(gulp.dest(libMinDest));
+//««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+// DELETE task  (Clean production and some development contents )
+//««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+gulp.task('clean', function () {
+  return del ([
+    './production/',
+    './development/css/**',
+    './development/javascript/**',
+    './development/lib/**',
+    './development/maps/'
+  ]);
+ 
 });
 
-//«««««««««««««««««««««««««««««
-// Uglify Task (for production)
+//////////////////////////////////////////////////////////
+/* «««««««««««««««« PRODUCTION ««««««««««««««««««««««««*/
+//////////////////////////////////////////////////////////
+
+
+// Javascript Uglify Task (for production)
 //«««««««««««««««««««««««««««««
 // File Paths
 var js2uglify = ['./development/javascript/script.js'];
@@ -89,66 +135,93 @@ var uglyJsDest = './production/javascript/';
 // Task
 gulp.task('uglifyJs', function(){
    gulp.src(js2uglify)
+   .pipe(plumber({            // plumber
+        errorHandler: onError
+    }))
      .pipe(uglify())
      .pipe(gulp.dest(uglyJsDest));  
 });
 
-//««««««««««««««««««««««««««««
-// Compass Task (development)
-//««««««««««««««««««««««««««««
-// Sass file path
-var sassSrc = ['./development/components/sass/style.scss'];
-var sassDest = './development/css/';
-// Task
-gulp.task('compass', function(){
-   gulp.src(sassSrc)
-      .pipe(plumber({
-        errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-      }}))
-      .pipe(compass({
-        config_file: './config.rb',
-        css: './development/css',
-        sass: './development/components/sass',
-        javascript: './development/javascript',
-        image: './development/components/images',
-        style: 'expanded',
-        comments: true,
-        require: ['susy']
-      }))
-      .on('error', function(err) {
-        console.log(err);
-        this.emit('end');
-      }) 
-      .pipe(autoprefixer('last 2 versions'))
-      .pipe(gulp.dest(sassDest))
-      .pipe(reload({stream:true}));
+
+// Concatenation of all the minifies javaScripts (for production)
+//«««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+// JS Libraries Minified files path
+var libMinSrc = [
+    './components/lib/angular/angular.min.js', 
+    './components/lib/angular-resource/angular-resource.min.js',
+    './components/lib/angular-route/angular-route.min.js',
+    './components/lib/jquery_min/index.js',
+    './components/lib/jquery-migrate_min/index.js',
+    './components/lib/bootstrap/dist/js/bootstrap.min.js'
+];
+var libMinDest = './production/lib/js';
+//Task
+gulp.task('concatMinLibs', function(){
+  gulp.src(libMinSrc)
+    .pipe(plumber({            // plumber
+        errorHandler: onError
+    }))
+    .pipe(concat('libraries.js'))
+    .pipe(plumber({            // plumber
+        errorHandler: onError
+    }))
+    .pipe(gulp.dest(libMinDest));
 });
 
-//««««««««««««««««««««««««««««««««««
+
+// Concatenation of all the minifies CSS Libraries (for production)
+//«««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+// CSS Libraries Minified files path
+var libMinCssSrc = [
+    './components/lib/bootstrap/dist/css/bootstrap.min.css'
+];
+var libMinCssDest = './production/lib/css/';
+//Task
+gulp.task('concatMinCssLibs', function(){
+   gulp.src(libMinCssSrc)
+     .pipe(plumber({            // plumber
+        errorHandler: onError
+     }))
+     .pipe(concat('libraries.css'))
+     .pipe(gulp.dest(libMinCssDest));
+});
+
+
 // gulp minify CSS (for Production)
 //««««««««««««««««««««««««««««««««««
 var cssSrc = 'development/css/style.css';
-var cssDest = 'production/css';
+var cssDest = 'production/css/';
 //Task
-gulp.task('minifyCSS', function(){
-  gulp.src(cssSrc)
-    .pipe(minifyCSS())
+gulp.task('minifyCss', function(){
+  return gulp.src(cssSrc)
+    .pipe(plumber({            // plumber
+        errorHandler: onError
+    }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest(cssDest));
 });
 
+
+// Minify HTML task
 //««««««««««««««««««««
-// gulp HTML task
-//««««««««««««««««««««
-var htmlSrc = './development/index.html';
+var htmlSrc = ['./development/index.html'];
+var htmlDest = './production';
 // Task
-gulp.task('html', function(){
-  gulp.src(htmlSrc)
-  .pipe(reload({stream:true}));
+gulp.task('minifyHTML', function(){
+  return gulp.src(htmlSrc)
+  .pipe(plumber({            // plumber
+        errorHandler: onError
+    }))
+  .pipe(htmlmin({collapseWhitespace: true, comment: true}))
+  .pipe(gulp.dest(htmlDest));
 });
 
-//«««««««««««««««««««««««««««««««««
+
+
+//////////////////////////////////////////////////////////
+/* ««««««««««««««« BROWSER SYNC (SERVER) «««««««««««««««*/
+//////////////////////////////////////////////////////////
+
 // Browser Sync task (Development)
 //«««««««««««««««««««««««««««««««««
 gulp.task('serverDev', function() {
@@ -170,34 +243,29 @@ gulp.task('serverProd', function() {
     });
 });
 
-//««««««««««««««««««««««««««««««««««
-// rimraf task  (Clean producion Directory)
-//««««««««««««««««««««««««««««««««««
-gulp.task('clean', function (cb) {
-rimraf('./production', cb);
-});
 
-//««««««««««««««««««««
+//////////////////////////////////////////////////////////
+/* «««««««««««««««« WATCH & DEFAULT TASK««««««««««««««««*/
+//////////////////////////////////////////////////////////
+
 // gulp Watch task
 //««««««««««««««««««««
 gulp.task('watch', function(){
-  gulp.watch('./development/components/javascript/**/*.js', ['concatJs', 'uglifyJs']);
-  gulp.watch('./development/components/sass/**/*.scss', ['compassDev', 'minifyCSS']);
-  gulp.watch('./gulpfile.js', ['default']);
-  gulp.watch('./development/**/*.html', ['html', 'minifyHTML']);
-
+  gulp.watch('./components/javascript/**/*.js', ['concatJs']);
+  gulp.watch('./components/sass/**/*.scss', ['sass']);
+  gulp.watch('./development/**/*.html', ['html']);
+  //gulp.watch('./gulpfile.js', ['default']);
 });
 
 
-//««««««««««««««««««««
 // gulp Default task
 //««««««««««««««««««««
-gulp.task('default', ['concatJs', 'concatLibs', 'concatMinLibs', 'uglifyJs', 'compass', 'minifyCSS', 'html', 'serverDev']);
+gulp.task('default', ['sass', 'html', 'concatJs', 'concatLibs', 'concatCssLibs', 'watch', 'serverDev']);
 
+gulp.task('production', ['uglifyJs', 'concatMinLibs', 'minifyCSS', 'concatMinCssLibs', 'minifyHTML', 'serverProd']);
 
-
-
-
+/*  */
+/*««««««««««««««««««««««««««««««««««««««««««««««««*/
 
 
 
